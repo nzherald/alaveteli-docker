f144b755202c00adcb6f17baf4a1168fe3f236c7
Caleb <caleb.tutty@nzherald.co.nz>
2015-04-02 11:33:44 +1300

    Include xapian configuration

--- assets/setup.sh
+++ assets/setup.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
 cd /opt/alaveteli
+
+rm -rf lib/acts_as_xapian/xapiandbs/production
+
+mkdir -p $XAPIAN_MOUNT_PATH/$RAILS_ENV
+ln -s $XAPIAN_MOUNT_PATH/$RAILS_ENV lib/acts_as_xapian/xapiandbs/$RAILS_ENV
+
+bundle exec rake db:create
+bundle exec rake db:migrate
 bundle exec rake themes:install
 bundle exec rake assets:precompile
-bundle exec rake db:migrate
-bundle exec rake xapian:rebuild_index models="PublicBody User InfoRequestEvent"
+
+# Uncomment to rebuild the index on every launch of the container
+# bundle exec rake xapian:rebuild_index models="PublicBody User InfoRequestEvent"
+
+chown -R $(whoami) /data
 
 bundle exec unicorn_rails -c ./config/unicorn.rb
